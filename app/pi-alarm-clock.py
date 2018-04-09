4661ed081497b207142f3aa599f55f2257d1e068
ThomasLamb <ThomasLamb@users.noreply.github.com>
2018-04-08 21:21:59 -0400

    More template format changes

--- app/pi-alarm-clock.py
+++ app/pi-alarm-clock.py
@@ -9,21 +9,21 @@ app = Flask(__name__, static_folder='static')
 @app.route('/index')
 @app.route('/home')
 def index():
-	now = datetime.datetime.now()
-	currentTimeString = now.strftime("%Y-%m-%d %H:%M")
-	tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-	tomorrowDayName = tomorrow.strftime("%A")
-	tomorrowAlarmTimeString = '06:45:00'
+    now = datetime.datetime.now()
+    currentTimeString = now.strftime('%A, %B %-d, %Y %-H:%M %S %p')
+    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+    tomorrowDayName = tomorrow.strftime("%A")
+    tomorrowAlarmTimeString = '06:45:00'
 
-	templateData = {
-		'application': 'Pi Alarm Clock',
-		'title': 'Main',
-		'currentTime': currentTimeString,
-		'alarmDay': tomorrowDayName,
-		'alarmTime': tomorrowAlarmTimeString
-	}
-	return render_template('main.html', **templateData)
+    templateData = {
+        'application': 'Pi Alarm Clock',
+        'title': 'Main',
+        'currentTime': currentTimeString,
+        'alarmDay': tomorrowDayName,
+        'alarmTime': tomorrowAlarmTimeString
+    }
+    return render_template('main.html', **templateData)
 
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
