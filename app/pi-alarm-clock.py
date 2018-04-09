e43fa1411a9caba695776704bce0d9c4ad9bc9b1
ThomasLamb <ThomasLamb@users.noreply.github.com>
2018-04-08 20:49:51 -0400

    Change spaces to tabs

--- app/pi-alarm-clock.py
+++ app/pi-alarm-clock.py
@@ -9,19 +9,19 @@ app = Flask(__name__, static_folder='static')
 @app.route('/index')
 @app.route('/home')
 def index():
-    now = datetime.datetime.now()
-    timeString = now.strftime("%Y-%m-%d %H:%M")
+	now = datetime.datetime.now()
+	timeString = now.strftime("%Y-%m-%d %H:%M")
 	tomorrow = datetime.date.today() + datetime.timedelta(days=1)
 	tomorrowDayName = tomorrow.strftime("%A")
 
-    templateData = {
-        'application': 'Pi Alarm Clock',
-        'title': 'Main',
-        'time': timeString,
+	templateData = {
+		'application': 'Pi Alarm Clock',
+		'title': 'Main',
+		'time': timeString,
 		'day': tomorrowDayName
-    }
-    return render_template('main.html', **templateData)
+	}
+	return render_template('main.html', **templateData)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+	app.run(host='0.0.0.0', port=80, debug=True)
