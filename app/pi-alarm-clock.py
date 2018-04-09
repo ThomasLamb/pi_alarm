edef4b20a41158e8e5b47413b03eb755ce380dd2
ThomasLamb <ThomasLamb@users.noreply.github.com>
2018-04-08 20:47:01 -0400

    Add form to set alarm for tomorrow

--- app/pi-alarm-clock.py
+++ app/pi-alarm-clock.py
@@ -1,22 +1,27 @@
-from flask import Flask, render_template
 import datetime
+
+from flask import Flask, render_template
+
 app = Flask(__name__, static_folder='static')
 
-from app import controller
 
-"""
 @app.route("/")
-def hello():
-	now = datetime.datetime.now()
-	timeString = now.strftime("%Y-%m-%d %H:%M")
-	templateData = {
-		'application': 'Pi Alarm Clock',
-		'title': 'Main',
-		'time': timeString
-	}
-	return render_template('main.html', **templateData)
+@app.route('/index')
+@app.route('/home')
+def index():
+    now = datetime.datetime.now()
+    timeString = now.strftime("%Y-%m-%d %H:%M")
+	tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+	tomorrowDayName = tomorrow.strftime("%A")
+
+    templateData = {
+        'application': 'Pi Alarm Clock',
+        'title': 'Main',
+        'time': timeString,
+		'day': tomorrowDayName
+    }
+    return render_template('main.html', **templateData)
 
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=80, debug=True)
-"""
+    app.run(host='0.0.0.0', port=80, debug=True)
